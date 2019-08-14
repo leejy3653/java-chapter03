aa5310530a028438603f3b1c996fc85824f9ec5e
bit <bit@DESKTOP-1LOO2I6>
2019-08-14 14:22:25 +0900

    init commit

---
+++ src/chapter03/StringTest01.java
@@ -0,0 +1,14 @@
+package chapter03;
+
+public class StringTest01 {
+	public static void main(String[] args) {
+		// c:\temp
+		System.out.println("c:\temp");
+
+		// \t => tab
+		// \n => new line
+		// \r => carriage return
+
+	}
+
+}
