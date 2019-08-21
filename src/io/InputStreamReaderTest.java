0c451efb473be6b512d5a0d560f3b6d607aba2ef
bit <bit@DESKTOP-1LOO2I6>
2019-08-21 09:14:25 +0900

    2019.08.21.		09:14

---
+++ src/io/InputStreamReaderTest.java
@@ -0,0 +1,37 @@
+package io;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+public class InputStreamReaderTest {
+	public static void main(String[] args) {
+		InputStreamReader isr = null;
+		try {
+			isr = new InputStreamReader(new FileInputStream("ms949.txt"), "MS949");
+			
+			int data = -1;
+			while((data = isr.read()) != -1) {
+				System.out.print((char)data);
+			}
+		} catch(FileNotFoundException e) {
+			System.out.println("File Not Found:" + e);
+		} catch (UnsupportedEncodingException e) {
+			System.out.println("Unsupported Encoding:" + e);
+		} catch (IOException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if(isr != null) {
+					isr.close();
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+}
